0c1687bc03d0875d9eec130ec6e997b0b09e04b9
Mahanga Hamilton <darryl.mahanga@aninf.ga>
2024-10-23 15:11:56 +0100

    maj

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
